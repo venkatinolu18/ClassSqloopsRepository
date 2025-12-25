74325c28b8bc3c2962c1241f7942d02b4cfb3959
venkat <inoluvenkat55@gmail.com>
2025-12-25 18:33:58 +0530

    Add .gitignore and remove venv/cache files

--- db/MySqlConnection.py
+++ db/MySqlConnection.py
@@ -5,9 +5,10 @@ class MySqlConnection:
         self.connection = mysqlConn.connect(
             host='localhost',
             user='root',
-            password='password',
+            password='Inolu@18',
             database='companydb'
         )
 
     def get_connection(self):
         return self.connection
+    
